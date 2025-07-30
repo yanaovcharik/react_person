f8e0c7fd0dde37cae87c65168cd2854448dc5717
Yana Ovcharyk <yanaovcharik@gmail.com>
2025-07-30 22:30:25 +0300

    .

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -16,7 +16,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {age !== undefined && <p className="Person__age">I am {age}</p>}
+      <p className="Person__age">
+        {person.age ? `I am ${age}` : 'I am ageless'}
+      </p>
       <p className="Person__partner">{partnerText}</p>
     </section>
   );
